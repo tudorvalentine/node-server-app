7cf1a85cb0993ee368082506eddbd97edaaf23e1
tudorvalentine <tudor1903@gmail.com>
2022-06-12 22:33:05 +0300

    Update to 2.0.0
    Implement auth and reg

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+const Router = require('express')
+const router = new Router();
+const controller = require('../controller/db_controller')
+
+router.post('/register', controller.createUser)
+router.post('/login', controller.login)
+router.get('/display', controller.displayUsers)
+router.get('/display/:id', controller.displayUser)
+
+module.exports = router
