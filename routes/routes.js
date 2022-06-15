e795900edf3395e0414a76379d024f83a0b9e13e
tudorvalentine <tudor1903@gmail.com>
2022-06-15 17:40:08 +0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -6,11 +6,7 @@ router.post('/register', controller.createUser)
 router.post('/login', controller.login)
 router.get('/display', controller.displayUsers)
 router.get('/display/:id', controller.displayUser)
-<<<<<<< Updated upstream
-router.get('/',controller.main)
-=======
 router.post('/displayPath/:id', controller.displayPath)
 router.post('/images/:nameimage', controller.getImage)
->>>>>>> Stashed changes
 
 module.exports = router
