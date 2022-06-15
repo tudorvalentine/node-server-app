0b44255bb6c7f35697466e8941aedc24370f979e
tudorvalentine <tudor1903@gmail.com>
2022-06-15 17:35:48 +0300

    Update to 2.1.0
    Working with table image_conspect DB. Implement download file.

--- controller/db_controller.js
+++ controller/db_controller.js
@@ -1,6 +1,7 @@
 const { json } = require("express/lib/response");
 const db = require("../db");
 const cryp = require("../crypt/crypto");
+const path = require("path")
 
 class DBController {
   async createUser(req, res) {
@@ -47,12 +48,16 @@ class DBController {
     );
   }
   async login(req, res) {
-    const { email, password } = req.body;
+    const {id , email, password } = req.body;
     console.log({
       action: "login",
+      id_client : id,
       email: email,
       pass: password
     });
+
+    let flag = false;
+    let id_u,userN,mail
     const user = await db.query(
       `SELECT * FROM USERS WHERE email = $1`,
       [email],
@@ -73,22 +78,45 @@ class DBController {
           const encryptedPass = result.rows[0].password;
           const decryptedPass = cryp.decrypt(encryptedPass);
           if (decryptedPass == password) {
-            const userAuth = {
-              error: false,
-              id_user: result.rows[0].id_user,
-              username: result.rows[0].username,
-              email: result.rows[0].email,
-            };
-            res.json(userAuth);
+            id_u = result.rows[0].id_user
+            userN = result.rows[0].username
+            mail = result.rows[0].email
+            flag = true
           } else res.json({ error: true, error_msg: "Parola incorecta" });
         } else res.json({ error: true, error_msg: "Date incorecte" });
       }
-    );
+    )
+
+    await db.query(`select image_conspect.id_assoc, image_conspect.image_path,image_conspect.conspect_path 
+    from users
+    inner join image_conspect on users.id_user = image_conspect.id_user 
+    where users.email = $1`,[email],(err,result)=>{
+      if (err) {
+        res.json({
+          error : true,
+          error_msg : "Error get path of source"
+        })
+      }else{
+        if(flag){
+          const userAuth = {
+            error: !flag,
+            id_user: id_u,
+            username: userN,
+            email: mail,
+            association : result.rows
+          }
+          res.json(userAuth);
+        }
+      }
+    })
   }
 
   async displayUsers(req, res) {
     const user = await db.query("SELECT * FROM users");
-    res.json(user.rows);
+    res.json({
+      id : 345,
+      assoc : user.rows
+    })
   }
 
   async displayUser(req, res) {
@@ -96,9 +124,29 @@ class DBController {
     const user = await db.query(`SELECT * FROM users where id_user = $1`, [id]);
     res.json(cryp.decrypt(user.rows[0].password));
   }
-
-  main(req , res){
-    res.send('<h2>It is my server for Android Application (Augmented Image)</h2>');
+  async displayPath(req, res){
+    const id = req.params.id
+    const pathImage = path.join(path.dirname(__dirname), 'image/db.jpg')
+    const pathConsp = path.join(path.dirname(__dirname), 'conspect/db.pdf')
+    console.log(pathImage)
+    console.log(pathConsp)
+    const assoc = await db.query(`insert into image_conspect (image_path,conspect_path,id_user) values ($1,$2,$3)`,[pathImage,pathConsp,id], (error, result) =>{
+      if (error) {
+        res.json({
+          err:"error",
+          msg : "Nu s-a primit"
+        })
+      }else{
+        res.json(result)
+      }
+    })
+  }
+  getImage(req, res){
+      const nameimage = req.params.nameimage
+      console.log(nameimage)
+      const pathImage = path.join(path.dirname(__dirname), 'images/',nameimage)
+      console.log(pathImage)
+      res.download(pathImage,nameimage)
   }
 }
 
--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const Pool = require('pg').Pool
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   password: 'tudorvalentine',
-//   port: 3000,
-//   database: 'augmentedimage_postgres'
-// })
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  user: 'postgres',
+  host: 'localhost',
+  password: 'tudorvalentine',
+  port: 3000,
+  database: 'augmentedimage_postgres'
 })
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// })
 
 module.exports = pool
