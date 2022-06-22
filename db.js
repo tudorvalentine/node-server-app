8e790eb7eadf383bc1d798205887b72c6587d1e8
tudorvalentine <tudor1903@gmail.com>
2022-06-22 20:19:43 +0300

    Modify connection

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  password: 'tudorvalentine',
-  port: 3000,
-  database: 'augmentedimage_postgres'
-}) || new Pool({
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   password: 'tudorvalentine',
+//   port: 3000,
+//   database: 'augmentedimage_postgres'
+// }) || new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
