5f432eedc16ee0e2f6582354f08bb0bc365b6f56
tudorvalentine <66331277+tudorvalentine@users.noreply.github.com>
2022-06-23 09:40:09 +0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const controller = require('../controller/db_controller')
 router.post('/register', controller.createUser)
 router.post('/login', controller.login)
 router.get('/images/:nameimage', controller.getImage)
+router.get('/pdf/:pdf', controller.getImage)
 
 router.post('/sync/:userId', controller.syncronize)
 
